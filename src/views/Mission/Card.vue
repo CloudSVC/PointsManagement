a36f43546840135b9da4ee8d2bf0c53582b401e4
nndty <nndty136@outlook.com>
2024-06-21 23:36:49 +0800

    第二次提交，本次提交是测试不同分支commit不同有什么区别

--- src/views/Mission/Card.vue
+++ src/views/Mission/Card.vue
@@ -6,7 +6,9 @@ export default {
                 "Mission 1",
                 "Mission 2",
                 "Mission 3",
-                "Mission 4"
+                "Mission 4",
+                "Mission 5",
+                "Mission 6"
             ]
         }
     },
